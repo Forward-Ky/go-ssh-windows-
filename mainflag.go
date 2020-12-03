bb02710d2157d582b07cffd89e63ae74c519396a
kouyan1 <506625166@qq.com>
2020-12-03 21:21:13 +0800

    new cli mode

---
+++ mainflag.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func main() {
+	// 建立SSH客户端连接
+
+	var ipport string
+	var username string
+	var pwd string
+	flag.Usage = func() {
+		flag.StringVar(&ipport, "ipport", "", "ip:端口")
+		flag.StringVar(&username, "username", "", "username")
+		flag.StringVar(&pwd, "password", "", "password")
+		flag.PrintDefaults()
+		fmt.Println(`eg: -ipport="10.1.1.1:22 -username="xxxx" -password="xxxxxx"`)
+	}
+	flag.Parse()
+
+	client, err := ssh.Dial("tcp", ipport, &ssh.ClientConfig{
+		User:            username,
+		Auth:            []ssh.AuthMethod{ssh.Password(pwd)},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Timeout:         20 * time.Second,
+		Config: ssh.Config{
+			Ciphers: []string{"aes128-ctr", "aes192-ctr", "aes256-ctr", "aes128-gcm@openssh.com",
+				"arcfour256", "arcfour128", "aes128-cbc", "aes256-cbc", "3des-cbc", "des-cbc",
+			},
+		},
+	})
+	if err != nil {
+		log.Fatalf("SSH dial error: %s", err.Error())
+	}
+
+	// 建立新会话
+	session, err := client.NewSession()
+	defer session.Close()
+	if err != nil {
+		log.Fatalf("new session error: %s", err.Error())
+	}
+
+	session.Stdout = os.Stdout // 会话输出关联到系统标准输出设备
+	session.Stderr = os.Stderr // 会话错误输出关联到系统标准错误输出设备
+	session.Stdin = os.Stdin   // 会话输入关联到系统标准输入设备
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          0,     // 禁用回显（0禁用，1启动）
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, //output speed = 14.4kbaud
+	}
+	if err = session.RequestPty("linux", 32, 160, modes); err != nil {
+		log.Fatalf("request pty error: %s", err.Error())
+	}
+	if err = session.Shell(); err != nil {
+		log.Fatalf("start shell error: %s", err.Error())
+	}
+	if err = session.Wait(); err != nil {
+		log.Fatalf("return error: %s", err.Error())
+	}
+}
